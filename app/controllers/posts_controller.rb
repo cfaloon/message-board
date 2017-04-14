3923d316eaffd0d192f94d3a69816c0159af0cf6
cfaloon <cfaloon@gmail.com>
2017-04-14 11:21:37 -0400

    The basis of new post creation and showing.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+
+  # GET /posts/new
+  def new
+    @post = Post.new(user_id: current_user.id)
+  end
+
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Post successfully created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # parameter sanitizer
+  def post_params
+    params.require(:post).permit(:title, :content, :user_id)
+  end
+end
